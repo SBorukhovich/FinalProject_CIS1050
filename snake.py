81c8d9b52d81f80cf82a5e0765e30371d7c6944c
SBorukhovich <sborukhovich66@gmail.com>
2022-11-22 22:00:37 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -11,16 +11,22 @@ red = (255,0,0)
 yellow = (255,255,0) 
 orange= (255,165,0)
 blue = (0,0,255)
+green = (0,255,0)
+background = (180,45,190)
 
-width, height = 600,400
+width, height = 480,480
 
 game_display = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Snake Game")
 
 clock = pygame.time.Clock()
 
-snake_size = 16
+snake_size = 20
 snake_speed= 10
+
+GRID_WIDTH = height / snake_size
+GRID_HEIGHT = width / snake_size
+
 
 message_font = pygame.font.SysFont('ubuntu',30)
 score_font =  pygame.font.SysFont('ubuntu',20)
@@ -28,6 +34,13 @@ score_font =  pygame.font.SysFont('ubuntu',20)
 def print_score(score):
     text = score_font.render("Score: " + str(score), True, orange)
     game_display.blit(text, [0,0])
+
+def food_color():
+            option = random.choice(['G', 'G','R'])
+            if option =='G':
+                return (0,255,0)
+            else:
+                return (255,0,0)
     
 def draw_snake(snake_size, snake_pixels):
     for pixel in snake_pixels:
@@ -38,33 +51,37 @@ def run_game():
     game_close = False
     x = width/2
     y = height/2
+    COLOR = green
     x_speed= 0
     y_speed= 0
     snake_pixels = []
     snake_length = 1
-    food_x = round(random.randrange(0, width-snake_size) / 10.0) * 10.0
-    food_y = round(random.randrange(0, height-snake_size) / 10.0) * 10.0
+    food_x = random.randint(0, GRID_WIDTH -1) * snake_size
+    food_y = random.randint(0, GRID_HEIGHT -1) * snake_size
 
 
     while not game_over:
         while game_close:
             game_display.fill(black)
-            game_over_message = message_font.render("Game Over!", True, red)
-            game_display.blit(game_over_message, [width / 3, height / 3])
-            game_over_message2 = message_font.render("Press 1 to Exit or 2 to Restart", True, red)
-            game_display.blit(game_over_message2, [width / 4, height /2])
+            game_over_message = message_font.render("GAME OVER!", True, red)
+            game_display.blit(game_over_message, [width / 4, height / 3])
+            game_over_message2 = message_font.render("Press 1 to Exit", True, white)
+            game_display.blit(game_over_message2, [width/4,height/2])
+            game_over_message3 = message_font.render("Press 2 to Restart", True, white)
+            game_display.blit(game_over_message3, [width/4,height/4*3])
+            
             print_score(snake_length - 1)
             pygame.display.update()
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_1: #When user press 1 game ends
-                        game_over = True
-                        game_close = False
+                        pygame.quit()
+                        quit()
                     if event.key == pygame.K_2: #When user press 2 game restarts
                         run_game()
                     if event.type == pygame.QUIT:
-                        game_over = True
-                        game_close = False
+                        pygame.quit()
+                        quit()
                     
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -87,8 +104,9 @@ def run_game():
         x += x_speed
         y += y_speed
         
-        game_display.fill(black) 
-        pygame.draw.rect(game_display, orange, [food_x, food_y,snake_size, snake_size])
+        game_display.fill(background)
+         
+        pygame.draw.rect(game_display, COLOR, [food_x, food_y,snake_size, snake_size])
         
         snake_pixels.append([x,y])
         
@@ -101,13 +119,22 @@ def run_game():
         draw_snake(snake_size,snake_pixels)
         print_score(snake_length-1)
         
-        pygame.display.update()
         
-        if x == food_x and y == food_y: #when snake gets food
-            food_x = round(random.randrange(0, width-snake_size) / 10.0) * 10.0
-            food_y = round(random.randrange(0, height-snake_size) / 10.0) * 10.0
+        if x == food_x and y == food_y and COLOR ==green: #when snake gets food
+            food_x = random.randint(0, GRID_WIDTH -1) * snake_size
+            food_y = random.randint(0, GRID_HEIGHT -1) * snake_size
+            COLOR = food_color()
             snake_length +=1 
+        if x == food_x and y == food_y and COLOR ==red: #when snake gets food
+            food_x = random.randint(0, GRID_WIDTH -1) * snake_size
+            food_y = random.randint(0, GRID_HEIGHT -1) * snake_size
+            COLOR = food_color()
+            snake_length -=1 
+            del snake_pixels[0] 
+        if snake_length <0:
+            game_close = True
         clock.tick(snake_speed)
+        pygame.display.update()
     pygame.quit()
     quit()
 run_game()
